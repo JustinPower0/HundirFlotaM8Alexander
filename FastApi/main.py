fd36029befa6573c6e4f7a35979b293dd2ec8405
HassaanAziz2004 <adaw23haziz@inslaferreria.cat>
2025-10-09 16:37:16 +0200

    Creo la matriz inicializandola con 0 y luego creo el get para que el fetch lo pueda recojer y printear

--- FastApi/main.py
+++ FastApi/main.py
@@ -3,10 +3,30 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 
+# Funciones Normales
+
+def crearMatriz(dim):
+    matriz = []
+    for i in range(dim):
+        fila = []
+        for j in range(dim):
+            fila.append(0)
+        matriz.append(fila)
+    return matriz
+
+
+# Variable
+partida = {}
+barco1 = [1]
+barco2 = [2,2]
+barco3 = [3,3,3]
+barco4 = [4,4,4,4]
+barco5 = [5,5,5,5,5]
+dimencion = 7
+matriz = crearMatriz(dimencion)
+
 # Crear la aplicación
 app = FastAPI(title="Mi Projecto", version="0.0.1")
-
-partida = []
 
 # Lista de orígenes permitidos
 origins = [
@@ -23,10 +43,10 @@ app.add_middleware(
     allow_headers=["*"],    # permite todas las cabeceras
 )
 
-# Funciones
-@app.get("/")
+# Funciones FastApi
+@app.get("/matriz")
 def read_root():
-    return {"mensaje": "Hola alumnos!"}
+    return matriz
 
 @app.get("/saludo/{nombre}")
 def read_item(nombre: str):
